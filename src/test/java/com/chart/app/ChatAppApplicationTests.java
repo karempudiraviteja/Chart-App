31f454f5976e9ba3218cb5808459b7aa1044052c
karempudiraviteja <2200030562@kluniversity.in>
2025-09-06 20:24:14 +0530

    Initial commit: Spring Boot Chat Application

---
+++ src/test/java/com/chart/app/ChatAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chart.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
